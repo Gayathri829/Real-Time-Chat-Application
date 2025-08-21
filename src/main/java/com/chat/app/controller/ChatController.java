2e2cb178afef1d798e84367a0f145f3e2d18124f
Gayathri829 <gayathrijampani@hotmail.com>
2025-08-21 13:31:57 +0530

    Initial commit

---
+++ src/main/java/com/chat/app/controller/ChatController.java
@@ -0,0 +1,23 @@
+package com.chat.app.controller;
+
+import com.chat.app.model.ChatMessage;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ChatController {
+
+    @MessageMapping("/sendMessage") //maps web socket msgs to destination
+    @SendTo("/topic/messages") //andharki kanipinchali msgs okavela grp chat aytee..
+    public ChatMessage sendMessage(ChatMessage message){
+        return message;
+    }
+
+    @GetMapping("chat")
+    public String chat(){
+        return "chat";
+    }
+
+}
